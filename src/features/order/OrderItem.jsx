599f02b277fd8ef9b100747093c0932e1415a4ba
Dmytro Harmash <dm.harmash@gmail.com>
2025-08-10 10:45:36 +0200

    files were sort, folders were create, ready to work

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../utilities/helpers";
+
+// eslint-disable-next-line
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
